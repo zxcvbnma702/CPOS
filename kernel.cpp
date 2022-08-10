83d2910f7af96ff45673d7ab69ac3f701ec01090
SunShibo <2990452909@qq.com>
2022-08-10 20:12:52 +0800

    add types.h

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,10 @@
+#include "types.h"
+
+
 void printf(const char* str) {
     //获取显示器地址
     //实模式下文本模式
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
     for (int i = 0; str[i]; i++) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     }
@@ -19,7 +22,7 @@ extern "C" void callConstructors(){
 
 
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     printf((char*)"hello world!");
     while(1);
 }

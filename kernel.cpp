91be21cc4d97f16fb232da1a404f2384b303c99c
sunshibo <2990452909@qq.com>
2022-08-13 21:55:40 +0800

    fix printf

--- kernel.cpp
+++ kernel.cpp
@@ -4,9 +4,37 @@
 void printf(const char* str) {
     //获取显示器地址
     //实模式下文本模式
+    //一页最大是 80 * 25 个字符
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    static uint8_t x = 0, y = 0;
+
     for (int i = 0; str[i]; i++) {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        switch(str[i]) {
+            case '\n':
+                y++;
+                x = 0;
+                break;
+            default:
+                VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        //换行
+        if(x >= 80){
+            x = 0;
+            y++;
+        }
+
+        //换页
+        if(y >= 25){
+            for (y = 0; y < 25; y++) {
+                for (x = 0; x < 80; x++) {
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+                }
+            }
+            x = 0, y = 0;
+        }
     }
 }
 
@@ -20,9 +48,8 @@ extern "C" void callConstructors(){
     }
 }
 
-
-
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
-    printf((char*)"hello world!");
+    printf((char*)"hello world!\n");
+    printf((char*)"world!");
     while(1);
 }
